3c31f1919bde8c254e78fe7e2aad1dc0be393cda
Initial1D <jaringan09@gmail.com>
2021-09-25 17:22:34 +0700

    python code

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 """
 semua sintaksis dasar bahasa pemograman terdiri dari:
-sekuensial : langkah berurutan
+sequential : langkah berurutan
 percabangan : langkah melompat jika kondisi terpenuhi
 perulangan : mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 """
-print("hallo word")
+# sequential
+
+print("belajar selagi ada waktu")
 print("ayo belajar!")
+
+# percabangan
+jumlah_botol_susu = 123
+
+if jumlah_botol_susu > 0:
+    print("beli 1 botol susu")
+else:
+    print("botol susu tidak dibeli")
+
+
