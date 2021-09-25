dbcc8701401acde6c80b6ec3408ed553642c9660
Initial1D <jaringan09@gmail.com>
2021-09-25 16:14:43 +0700

    python code

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 """
-ini adalah project python percobaan
+semua sintaksis dasar bahasa pemograman terdiri dari:
+sekuensial : langkah berurutan
+percabangan : langkah melompat jika kondisi terpenuhi
+perulangan : mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 """
 print("hallo word")
 print("ayo belajar!")
